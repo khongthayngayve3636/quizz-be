7fcec5ccce5969fbe278913a1991021886799d4e
onlyflan <qalc2003@gmail.com>
2026-06-28 22:14:45 +0700

    fix(auth): fix JWT_SECRET typing for typescript build

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET: string = process.env.JWT_SECRET as string;
 if (!JWT_SECRET) {
   throw new Error("FATAL ERROR: JWT_SECRET environment variable is not defined.");
 }
